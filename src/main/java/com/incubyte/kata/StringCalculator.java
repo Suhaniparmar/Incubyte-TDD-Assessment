2757c6916b5666ec364a675a2963a2c0104a7d4b
suhani <1008suhaniparmar@gmail.com>
2025-07-04 01:20:43 +0530

    [step-2] Refactor : added separate method for single and multiple numbers

--- src/main/java/com/incubyte/kata/StringCalculator.java
+++ src/main/java/com/incubyte/kata/StringCalculator.java
@@ -2,9 +2,13 @@ package com.incubyte.kata;
 
 public class StringCalculator {
     public static int add(String input) {
-        if(input.equals("")){
+        if (input == null || input.trim().isEmpty()){
             return 0;
         }
-        return Integer.parseInt(input);
+        return parseSingleNumber(input);
+    }
+
+    private static int parseSingleNumber(String input){
+        return Integer.parseInt(input.trim());
     }
 }
