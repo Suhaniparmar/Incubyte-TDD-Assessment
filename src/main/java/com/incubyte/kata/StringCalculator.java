e2f3ad2fe2713fc813853c9b34ca5e5d4f035ed8
suhani <1008suhaniparmar@gmail.com>
2025-07-04 01:43:49 +0530

    [step-3] Refactor : extract sumTwoNumbers() method

--- src/main/java/com/incubyte/kata/StringCalculator.java
+++ src/main/java/com/incubyte/kata/StringCalculator.java
@@ -1,13 +1,12 @@
 package com.incubyte.kata;
 
 public class StringCalculator {
-    public static int add(String input) {
+    public static int add(String input){
         if (input == null || input.trim().isEmpty()){
             return 0;
         }
-        if (input.contains(",")) {
-            String[] parts = input.split(",");
-            return Integer.parseInt(parts[0].trim()) + Integer.parseInt(parts[1].trim());
+        if (input.contains(",")){
+            return sumTwoNumbers(input);
         }
         return parseSingleNumber(input);
     }
@@ -16,4 +15,10 @@ public class StringCalculator {
         return Integer.parseInt(input.trim());
     }
 
+    private static int sumTwoNumbers(String input){
+        String[] parts = input.split(",");
+        int num1 = parseSingleNumber(parts[0]);
+        int num2 = parseSingleNumber(parts[1]);
+        return num1 + num2;
+    }
 }
