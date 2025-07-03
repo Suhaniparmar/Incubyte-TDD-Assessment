61a519c414cc078faa71bc27855f6e287c924169
suhani <1008suhaniparmar@gmail.com>
2025-07-04 01:39:24 +0530

    [step-3] Feat : split input on comma and sum values

--- src/main/java/com/incubyte/kata/StringCalculator.java
+++ src/main/java/com/incubyte/kata/StringCalculator.java
@@ -5,10 +5,15 @@ public class StringCalculator {
         if (input == null || input.trim().isEmpty()){
             return 0;
         }
+        if (input.contains(",")) {
+            String[] parts = input.split(",");
+            return Integer.parseInt(parts[0].trim()) + Integer.parseInt(parts[1].trim());
+        }
         return parseSingleNumber(input);
     }
 
     private static int parseSingleNumber(String input){
         return Integer.parseInt(input.trim());
     }
+
 }
